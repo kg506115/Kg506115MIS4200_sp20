0fce7e8676350892306dfce353981f92c382dc30
Gill <kg506115@ohio.edu>
2020-01-22 11:16:15 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kg506115MIS4200_sp20.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kg506115MIS4200_sp20.Startup))]
+namespace Kg506115MIS4200_sp20
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
